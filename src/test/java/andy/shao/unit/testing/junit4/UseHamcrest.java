639e1f794280a3ea70981a945f6050cdfb8724e9
Andy-Shao <hj13035952619@126.com>
2014-07-18 16:01:30 +0800

    all

--- src/test/java/andy/shao/unit/testing/junit4/UseHamcrest.java
+++ src/test/java/andy/shao/unit/testing/junit4/UseHamcrest.java
@@ -4,9 +4,12 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.hamcrest.Matchers;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class UseHamcrest {
@@ -27,6 +30,7 @@ public class UseHamcrest {
 		assertThat(color, is(not("white")));
 	}
 	
+	@Ignore
 	@Test
 	public void testHas(){
 		{
@@ -38,6 +42,29 @@ public class UseHamcrest {
 			List<Integer> ages = Arrays.asList(20, 30, 40);
 			assertThat(ages, not(Matchers.hasItem(Matchers.lessThan(18))));
 			assertThat(20, Matchers.isIn(ages));
+		}
+		
+		{
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("color", "red");
+			
+			assertThat(map, Matchers.<String,String>hasValue("red"));
+			assertThat(map, Matchers.<String,String>hasKey("color"));
+		}
+		
+		{
+			Object client = new Object(){
+				@SuppressWarnings("unused")
+                private String clientName;
+
+				public Object setClientName(String clientName) {
+                	this.clientName = clientName;
+                	return this;
+                }
+				
+			}.setClientName("Jane");
+			
+			assertThat(client, Matchers.hasProperty("clientName", is("Jane")));
 		}
 	}
 }
